da5923391517206652191e9bf18c5902038f3985
Nicolei Ocana <nicoleiocana@gmail.com>
2019-09-28 18:32:25 +0000

    Add user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
   # GET to /users/:user_id/profile/new
   def new
-    # Render blank profile deatils form
+    @profile = Profile.new
   end
 
 end
