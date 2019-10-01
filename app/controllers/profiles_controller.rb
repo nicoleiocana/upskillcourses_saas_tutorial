0a38d126a83b4acfaa14cf125e9997c6163c1e05
Nicolei Ocana <nicoleiocana@gmail.com>
2019-10-01 03:51:50 +0000

    Add users index action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
+  before_action :only_current_user
   
   # GET to /users/:user_id/profile/new
   def new
@@ -46,5 +47,9 @@ class ProfilesController < ApplicationController
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
-
+    
+    def only_current_user
+      @user = User.find(params[:user_id])
+      redirect_to root_path unless @user == current_user
+    end
 end
