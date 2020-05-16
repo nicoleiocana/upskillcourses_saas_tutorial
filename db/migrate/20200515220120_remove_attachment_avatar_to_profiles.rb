096e08475da7914d817078a5b1af0c2fa6215eb2
Nicolei Ocana <nicoleiocana@gmail.com>
2020-05-16 23:44:00 +0000

    Delete paperclip migrations

--- db/migrate/20200515220120_remove_attachment_avatar_to_profiles.rb
+++
@@ -1,9 +0,0 @@
-class RemoveAttachmentAvatarToProfiles < ActiveRecord::Migration[5.2]
-  def up
-    remove_attachment :profiles, :avatar
-  end
-
-  def down
-    add_attachment :profiles, :avatar
-  end
-end
