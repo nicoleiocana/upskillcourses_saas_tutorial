35f2d9573d9a45a04d56522cf19b0828dfaf9425
Nicolei Ocana <nicoleiocana@gmail.com>
2020-05-17 02:14:25 +0000

    Update SENDGRID_API_KEY

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   
   ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
     :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
