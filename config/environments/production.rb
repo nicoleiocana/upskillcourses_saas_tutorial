27b4e513f0529188bfe19676aadb7e782c03e251
Nicolei Ocana <nicoleiocana@gmail.com>
2020-05-17 16:27:02 +0000

    Update host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'nicoleiocana-samplesaas.herokuapp.com'
+  host = 'nicoleiocana-samplesaas2.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
