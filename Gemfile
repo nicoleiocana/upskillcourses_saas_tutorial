e9be81fd73e85b45ca479f139c20e841d89a027d
Nicolei Ocana <nicoleiocana@gmail.com>
2020-05-17 04:40:56 +0000

    Upgrade to Amazon SES

--- Gemfile
+++ Gemfile
@@ -62,8 +62,8 @@ gem 'image_processing', '~> 1.10', '>= 1.10.3'
 # Manipulate images with minimal use of memory via ImageMagick
 gem 'mini_magick', '~> 4.10', '>= 4.10.1'
 
-# Use ActionMailer with SendGrid's Web API
-gem 'sendgrid-actionmailer', '~> 3.0', '>= 3.0.2'
+# Official AWS Ruby gem for Amazon Simple Email Service (Amazon SES)
+gem 'aws-sdk-ses', '~> 1.29'
 
 group :development, :test do
   # Use sqlite3 as the database for Active Record
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,21 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.delivery_method = :sendgrid_actionmailer
-  config.action_mailer.sendgrid_actionmailer_settings = {
-      api_key: ENV['SENDGRID_API_KEY'],
-      raise_delivery_errors: true
-    }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'nicoleiocana-samplesaas.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: ENV['SMTP_ADDRESS'],
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+  
+  config.action_mailer.default_url_options = { host: 'yoursite-domain.com' }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
